069c66e069d526c4f917b947eb6677a5beeba90f
Ignasiya <ignasiya93@gmail.com>
2023-05-15 11:41:45 +0300

    rough draft

---
+++ html-img.py
@@ -0,0 +1,8 @@
+old_name = ['<div>', '<img data-u="image" src="img/1.JPG" />', '<img data-u="thumb" src="img/1.JPG" />', '</div>']
+
+with open('name.txt', 'w') as txt_file:
+    for i in range(1, 209):
+        print(old_name[0], file=txt_file)
+        print('\t' + str(old_name[1]).replace('1', str(i)), file=txt_file)
+        print('\t' + str(old_name[2]).replace('1', str(i)), file=txt_file)
+        print(old_name[3], file=txt_file)
---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+ 
+path = r"C:/Users/Inform4/Documents/GB/Krylovo-slider/img//"
+f = os.listdir(path)
+n = 0
+ 
+for i in f:
+    oldname = path + f[n]
+    newname = path + str(n + 1) + '.jpg'
+    os.rename(oldname, newname)
+    print(oldname, '======>', newname)
+    n += 1
