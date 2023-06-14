895030c94842094391c017677a33d136f4c9efa8
Ignasiya <ignasiya93@gmail.com>
2023-06-14 18:09:00 +0300

    v2

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
  
 path = r"C:/Users/Inform4/Documents/GB/Krylovo-slider/img/"
 f = os.listdir(path)
-n = 0
+n = 1
  
 for i in f:
-    oldname = path + f[n]
-    newname = path + str(n + 1) + '.jpg'
+    # name = 1000 + int(i.split()[0])
+    oldname = path + i
+    newname = path + str(n) + '.jpg'
     os.rename(oldname, newname)
     print(oldname, '======>', newname)
     n += 1
